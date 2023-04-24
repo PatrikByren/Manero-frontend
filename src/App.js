853c8af2411104e8786d47ba89ce94a29e1d4eca
Patrik Byren <patrik.byren@hotmail.com>
2023-04-24 10:41:21 +0200

    Ready and Clean

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomeView from './views/HomeView';
+
+function App() {
+  return (
+    <div className='container'>
+      <HomeView/>
+    </div>
+  );
+}
+
+export default App;
