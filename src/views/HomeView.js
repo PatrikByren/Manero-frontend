db1ee8129ca60f2afa9d423fb9ec605ceb78e317
carllohr <calle.lohr@gmail.com>
2023-04-25 13:41:37 +0200

    showcase

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "../components/sections/Header";
 import ProductCarousel from "../components/sections/ProductCarousel";
 import MenuBar from "../components/sections/MenuBar";
+import Showcase from "../components/sections/Showcase";
 
 const HomeView = () => {
   return (
     <div>
       <Header/>
+      <Showcase/>
       <ProductCarousel/>
       <MenuBar/>
     </div>
