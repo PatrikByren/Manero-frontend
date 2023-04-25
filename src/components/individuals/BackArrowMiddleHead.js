fb2d7880a81727de07ebcfb24417532745309e2e
Patrik Byren <patrik.byren@hotmail.com>
2023-04-25 15:22:57 +0200

    password delen påbörjad

--- src/components/individuals/BackArrowMiddleHead.js
+++ src/components/individuals/BackArrowMiddleHead.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const BackArrowMiddleHead = ({ content }) => {
   return (
-    <div className='arrowicon'>
-      <div className="arrow">
-        <i class="fa-solid fa-chevron-left"></i>
+    <div className='backarrowmiddlehead'>
+      <div className='arrowicon'>
+        <div className="arrow">
+          <i class="fa-solid fa-chevron-left"></i>
+        </div>
+        <div className='content'>{content}</div>
       </div>
-      <div className='content'>{content}</div>
     </div>
   )
 }
--- src/views/signInSignUpViews/ForgotPasswordView.js
+++ src/views/signInSignUpViews/ForgotPasswordView.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 import InputSingel from "../../components/individuals/InputSingel"
+import BackArrowMiddleHead from "../../components/individuals/BackArrowMiddleHead";
 
 const ForgotPasswordView = () => {
+  const [email, setEmail] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
   };
   return (
+    <div className="forgotpassword" >
       <form onSubmit={handleSubmit}>
-        <div className="inputcontainer">
-        <div className="text-center">
-          <InputSingel name="EMAIL" nameid="email" example="name@domain.com"/>
-          <button className="basebtn" type="submit">SEND</button>
+        <BackArrowMiddleHead content="Forgot password" />
+        <div>
+          <div className="content m-4">Please enter you email adress you will recive a link to create a new password via email</div>
+          <div className="inputcontainer">
+            <div className="text-center">
+              <InputSingel name="EMAIL" nameid="email" placeholder="name@domain.com" value={email} setValue={setEmail} />
+              <button className="basebtn" type="submit">SEND</button>
+            </div>
+          </div>
         </div>
-        </div>
-    </form>
-    
+      </form>
+    </div>
+
   )
 }
 
