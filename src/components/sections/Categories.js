af3ba4bb78c3151eeba13fb8fd188faaf47ef019
carllohr <calle.lohr@gmail.com>
2023-04-26 16:04:53 +0200

    searchview

--- src/components/sections/Categories.js
+++ src/components/sections/Categories.js
@@ -3,14 +3,28 @@ import React from "react";
 const Categories = () => {
   return (
     <section className="categories">
-      <div className="consumercategory">
+      <div className="consumercategories">
         <button className="consumerbtn">Men</button>
         <button className="consumerbtn">Women</button>
         <button className="consumerbtn">Kids</button>
         <button className="consumerbtn">Accesories</button>
       </div>
-      <div className="productcategory">
-        
+      <div className="productcategories">
+          <div className="dresses">
+            <p>Dresses</p>
+          </div>
+          <div className="pants">
+            <p>Pants</p>
+          </div>
+          <div className="accessories">
+            <p>Accesories</p>
+          </div>
+          <div className="shoes">
+            <p>Shoes</p>
+          </div>
+          <div className="t-shirts">
+            <p>T-Shirts</p>
+          </div>
       </div>
     </section>
   );
