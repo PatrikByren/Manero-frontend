d87b72560d919a92315e87579ecb5edf84754de4
Patrik Byren <patrik.byren@hotmail.com>
2023-04-26 09:32:21 +0200

    fixed App.css

--- src/components/individuals/ProfileIntro.js
+++ src/components/individuals/ProfileIntro.js
@@ -7,8 +7,8 @@ const ProfileIntro = () => {
         <div>
             <div className='lineup30'></div>
             {image ? (<div className='profileImage'><i class="fa-light fa-pen-line"></i></div>) : (<div> <img className='profileImage' src={image} alt="ProfileImages" /><i class="fa-light fa-pen-line"></i></div>)}
-            <div className='title-2 text-center'>Patrik Byren</div>
-            <div className='text-center'>patrik.byren@hotmail.com</div>
+            <div className='title-2 text-center'>Firstname Lastname</div>
+            <div className='text-center'>name@domain.com</div>
         </div>
     )
 }
