ba9ef8b30cdd4d775ecc417015e7758791b55fd8
FredrikJung <fredrik.jung@hotmail.com>
2023-04-26 15:28:04 +0200

    fix bugg v3

--- src/components/individuals/SideMenu.js
+++ src/components/individuals/SideMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
 const SideMenu = ({ icon, onclick }) => {
   // const [isActive, setIsActive] = new useState(false);
