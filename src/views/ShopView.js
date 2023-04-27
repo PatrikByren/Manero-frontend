a3e5cc61d9c23d305ed1453eb7dfa01e4b8e380f
jabe2201 <jacobwback@gmail.com>
2023-04-27 15:35:00 +0200

    Added select dropdown to ShopView Sort by

--- src/views/ShopView.js
+++ src/views/ShopView.js
@@ -4,47 +4,135 @@ import Product from "../components/individuals/Product";
 import MenuBar from "../components/sections/MenuBar";
 
 const ShopView = () => {
-    const location = useLocation();
-    const { from } = location.state;
-    var tag = from;
+  const location = useLocation();
+  const { from } = location.state;
+  var tag = from;
 
-    return(
-        <section className="d-flex flex-column container-lg">
-            <Header icon={"fa-regular fa-chevron-left"} title={tag} />
-            <div className="d-flex flex-column" style={{margin:8}}>
-                <div className="d-flex justify-content-between color-grey">
-                    <div className="d-flex align-items-center " >
-                        <i className="fa-light fa-sliders-up" style={{fontSize:13, marginBottom:15}}></i>
-                        <p style={{fontSize:14, marginLeft:5}}>Filter</p>
-                    </div>
-                    <div className="d-flex align-items-center" >
-                        <p style={{fontSize:14, marginRight:5}}>Sort by</p>
-                        <i className="fa-light fa-chevron-down" style={{fontSize:10, marginBottom:15}}></i>
-                    </div>
-                </div>
-                <div className="d-flex flex-wrap justify-content-between">
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                    <Product cardClass="card-3" price="40.50" discountPrice="30.50" product="Black Sneakers"/>
-                </div>
-                <MenuBar />
+  return (
+    <section className="d-flex flex-column container-lg">
+      <Header icon={"fa-regular fa-chevron-left"} title={tag} />
+      <div className="d-flex flex-column" style={{ margin: 8 }}>
+        <div className="d-flex justify-content-between color-grey">
+          <div className="d-flex align-items-center ">
+            <i
+              className="fa-light fa-sliders-up"
+              style={{ fontSize: 13, marginBottom: 15 }}
+            ></i>
+            <p style={{ fontSize: 14, marginLeft: 5 }}>Filter</p>
+          </div>
+          <div className="d-flex align-items-center">
+            <p style={{ fontSize: 14}}>Sort by</p>
+            <div className="sortBy">
+                <select className="form-select" aria-label="Default select example">
+                    <option className="selectOption" value="1">Popularity</option>
+                    <option className="selectOption" value="2">Price (from highest to lowest)</option>
+                    <option className="selectOption" value="3">Price (from lowest to highest)</option>
+                </select>
             </div>
-        </section>
-        
-    );
+          </div>
+        </div>
+        <div className="d-flex flex-wrap justify-content-between">
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+          <Product
+            cardClass="card-3"
+            price="40.50"
+            discountPrice="30.50"
+            product="Black Sneakers"
+          />
+        </div>
+        <MenuBar />
+      </div>
+    </section>
+  );
 };
 
 export default ShopView;
