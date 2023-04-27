7db030e9ca3976415a281fd4017e3a49d2b2e2d4
FredrikJung <fredrik.jung@hotmail.com>
2023-04-27 11:43:00 +0200

    Fixed links

--- src/components/sections/AccountCreated.js
+++ src/components/sections/AccountCreated.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "../../asset/images/userimg.png";
 import Header from "./Header";
+import { NavLink } from "react-router-dom";
 
 const AccountCreated = () => {
   return (
@@ -9,21 +10,23 @@ const AccountCreated = () => {
         <Header title="MANERO" />
       </div>
       <div className="media-border">
-      <div className="user-image">
-        <img src={Image} alt="User"></img>
-      </div>
-      <div className="d-flex justify-content-center mt-3">
-        <div className="lineup30"></div>
-      </div>
-      <div className="title-1">Account Created!</div>
-      <div>
-        <p className="text-center">
-          Your account had been created <br></br>successfully.
-        </p>
-      </div>
-      <button className="basebtn" type="submit">
-        SHOP NOW
-      </button>
+        <div className="user-image">
+          <img src={Image} alt="User"></img>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <div className="lineup30"></div>
+        </div>
+        <div className="title-1">Account Created!</div>
+        <div>
+          <p className="text-center">
+            Your account had been created <br></br>successfully.
+          </p>
+        </div>
+        <NavLink to="/">
+          <button className="basebtn" type="submit">
+            SHOP NOW
+          </button>
+        </NavLink>
       </div>
     </div>
   );
