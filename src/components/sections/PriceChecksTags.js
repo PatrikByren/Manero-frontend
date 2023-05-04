60b834bf3f80bb88900bc1c55fccf854437edff3
carllohr <calle.lohr@gmail.com>
2023-05-04 09:14:09 +0200

    some more on filter

--- src/components/sections/PriceChecksTags.js
+++ src/components/sections/PriceChecksTags.js
@@ -7,9 +7,14 @@ const PriceChecksTags = () => {
   return (
     <section className="pricecheckstagssection">
         <div className="price-container">
-            <p className="pricetext">Price</p>
+            <p className="pricetext">Max Price: </p>
             <input type="range" id="pricevalue" name="pricevalue" className="pricerange" min="50" max="2000" onChange={(e) => setInputPrice(e.target.value)} step="1"></input>
             <p className="price-display">${inputPrice}</p>
+        </div>
+        <div className="checkbox-container">
+        <div className="check-box"><input type="checkbox"/><p className="salesIcon">SALE</p></div>
+        <div className="check-box"><input type="checkbox"/><p className="salesIcon">SALE</p></div>
+        <div className="check-box"><input type="checkbox"/><p className="salesIcon">SALE</p></div>
         </div>
     </section>
   )
