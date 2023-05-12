f45a93491fc138589e53620689f21cf4ac633f1c
LeonardoAberg <leo.carqlvist@gmail.com>
2023-05-12 11:29:03 +0200

    asdfpayment

--- src/views/Paymentstuff/PaymentmethodView.js
+++ src/views/Paymentstuff/PaymentmethodView.js
@@ -18,31 +18,34 @@ const PaymentMethods = () => {
     };
 
     return (
-        <div className='media-border'>
-            <Header icon={"fa-solid fa-chevron-left"} title="Payment Method" />
-            <div className='paymentTest'>Cards
-                <div className='paymentTeste'>Add a new card  <i className='paymentPlus fa fa-plus' aria-hidden="true"></i></div>
-            </div>
-            <div className='paymentCardsPics'>
-                <img src={card1} alt="visakort" />
-                <div className='paymentCardsPicsMargin'>
-                    <img src={card2} alt="visakort" />
+        <div className="d-flex justify-content-center align-items-center vh-100">
+            <form onSubmit={handleSubmit} className="card shadow col-11 col-lg-4" noValidate>
+                <div className='paymentHeader'>
+                    <Header icon={"fa-solid fa-chevron-left"} title="Payment Method" />
                 </div>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Apple Payment
-                    <input type="text" value={payment} onChange={handlePaymentChange} className='inputPayment' />
-                </label>
-                <label>
-                    Pay Payl
-                    <input type="etext" value={payment} onChange={handlePaymentChange} className='inputPayment' />
-                </label>
-                <label>
-                    Apple Payment
-                    <input type="etext" value={payment} onChange={handlePaymentChange} className='inputPayment' />
-                </label>
-                <button className='formBtn' type="submit">Sign in</button>
+                <div className='paymentTest'>Cards
+                    <div className='paymentTeste'>Add a new card <i className='paymentPlus fa fa-plus' aria-hidden="true"></i></div>
+                </div>
+                <div className='paymentCardsPics'>
+                    <img src={card1} alt="visakort" />
+                </div>
+                <div className="card-body px-5 py-4">
+                    <div className="mb-3 small text-danger text-center">
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Apple Pay</label>
+                        <input value={payment} onChange={(e) => setPayment(e.target.value)} type="email" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Pay Pal</label>
+                        <input value={payment} onChange={(e) => setPayment(e.target.value)} type="password" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label className="form-label">Payoneer</label>
+                        <input value={payment} onChange={(e) => setPayment(e.target.value)} type="password" className="form-control" />
+                    </div>
+                </div>
             </form>
         </div>
     )
