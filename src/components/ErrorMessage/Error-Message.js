248e0b772b387035452b4eb162a8524f21316ff6
RobinFritzFredriksson <you@example.com>
2023-05-12 13:33:42 +0200

    clean and change

--- src/components/ErrorMessage/Error-Message.js
+++ src/components/ErrorMessage/Error-Message.js
@@ -1,7 +1,4 @@
-import React, { useState } from "react";
-
 function ErrorComponent({ errormessage, showError, setShowError }) {
-
   return (
     <div className="error-message">
       {!showError && <p className="error-message">{errormessage}</p>}
