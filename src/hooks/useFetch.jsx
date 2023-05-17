596dfb21387be2f787b3cc083a331d6590152883
carllohr <calle.lohr@gmail.com>
2023-05-17 12:33:45 +0200

    general usefetch hook added

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("could not fetch the data for that resource");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
+      });
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
