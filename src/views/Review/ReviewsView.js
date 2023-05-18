b13d754c7171049a4152260bbe6c9b9c07ea7051
LeonardoAberg <leo.carqlvist@gmail.com>
2023-05-18 15:32:27 +0200

    reviews klart

--- src/views/Review/ReviewsView.js
+++ src/views/Review/ReviewsView.js
@@ -1,20 +1,175 @@
-import React from 'react'
-import { useParams } from 'react-router-dom';
-import BackArrowMiddleHead from '../../components/individuals/BackArrowMiddleHead';
+import React from 'react';
+import profilimg from "../../asset/images/profilimg.png";
+import Header from '../../components/sections/Header';
 
 const Reviews = () => {
-  const {inputReview}=useParams();
   return (
-    <div>
-      <BackArrowMiddleHead content="Leave a review" />
-
-      <div>
-      <h2>Här ska reviewsen synas:</h2>
-      <p>{inputReview}</p>
-    </div>
-
+    <div className='container'>
+      <div className='m-4'>
+        <Header icon={"fa-solid fa-chevron-left"} title={"Reviews"} />
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
+      <div className='d-flex justify-content-center align-items-center'>
+        <div className='row'>
+          <div className='col-2'>
+            <img src={profilimg} alt="asdf" />
+          </div>
+          <div className='col-4'>
+            <p>Anette Black</p>
+          </div>
+          <div className='col-6'>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star checked"></span>
+            <span class="fa fa-star"></span>
+          </div>
+          <div className='col'>
+            <p>Consequat ut ea dolor aliqua laborum tempor Lorem culpa. Commodo veniam sint est mollit proident commodo.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Reviews
+export default Reviews;
