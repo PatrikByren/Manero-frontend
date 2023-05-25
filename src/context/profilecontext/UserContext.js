3940a91c514c2fe8823561b3932d527ddacdd6e9
Patrik Byren <patrik.byren@hotmail.com>
2023-05-25 09:49:09 +0200

    sync

--- src/context/profilecontext/UserContext.js
+++ src/context/profilecontext/UserContext.js
@@ -232,11 +232,10 @@ export const UserProvider = ({children}) => {
             const respnsData = await response.json();
             console.log(respnsData.addressList)
             setMyAddressList(await respnsData.addressList);
-            console.log(respnsData)
             if(response.status === 400){
-                console.log(respnsData.title)
+                setMyAddressList([]);
                 if (respnsData.title === undefined){
-                    console.log(respnsData.statusMessage)
+                    setMyAddressList([]);
                 }
             }
             return myAddressList;
