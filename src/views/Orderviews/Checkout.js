8fb0e6347bbd6605733ebd9f48e2187eec515545
carllohr <calle.lohr@gmail.com>
2023-05-25 16:37:26 +0200

    sync

--- src/views/Orderviews/Checkout.js
+++ src/views/Orderviews/Checkout.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import BackArrowMiddleHead from "../../components/individuals/BackArrowMiddleHead";
 import InputSingel from "../../components/individuals/InputSingel";
 import { useUserContext } from "../../context/profilecontext/UserContext";
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useCartContext } from "../../context/Shoppingcartcontext/CartContext";
 
 const Checkout = () => {
@@ -15,9 +15,10 @@ const Checkout = () => {
   const [shippingPostal, setShippingPostal] = useState("");
   const [shippingCity, setShippingCity] = useState("");
   const renderone = "";
-  useEffect(() => { // ta emot info från cartview
+  useEffect(() => {
+    // ta emot info från cartview
     const urlParams = new URLSearchParams(window.location.search);
-    const data = urlParams.get('data');
+    const data = urlParams.get("data");
     if (data) {
       const parsedData = JSON.parse(data);
       console.log(parsedData);
@@ -33,45 +34,50 @@ const Checkout = () => {
   );
 
   useEffect(() => {
-  if (shippingAddress) {
-    setShippingStreet(shippingAddress.streetName);
-    setShippingPostal(shippingAddress.postalCode);
-    setShippingCity(shippingAddress.city);
-  }
-  }, [shippingAddress])
+    if (shippingAddress) {
+      setShippingStreet(shippingAddress.streetName);
+      setShippingPostal(shippingAddress.postalCode);
+      setShippingCity(shippingAddress.city);
+    }
+  }, [shippingAddress]);
 
-  const url = 'https://manero.azurewebsites.net/api/Order';
+  const url = "https://manero.azurewebsites.net/api/Order";
   const data = {
     productItems: items,
     address: shippingStreet,
     city: shippingCity,
-    postalcode: shippingPostal
+    postalcode: shippingPostal,
   };
   const onSubmit = () => {
-    setIsLoading(true)
-    var storageToken = sessionStorage.getItem('token');
+    setIsLoading(true);
+    var storageToken = sessionStorage.getItem("token");
     console.log(shippingCity);
     axios
-      .post(url, data, { headers : { Authorization : `Bearer ${storageToken}`,
-      'Content-Type': 'application/json'} })
+      .post(url, data, {
+        headers: {
+          Authorization: `Bearer ${storageToken}`,
+          "Content-Type": "application/json",
+        },
+      })
       .then((response) => {
-        if (response.status === 201) { // skicka med infon till nya window
+        if (response.status === 201) {
+          // skicka med infon till nya window
           const newData = response.data;
           const params = new URLSearchParams();
-          params.append("data", JSON.stringify(newData)); 
-          setIsLoading(false)
+          params.append("data", JSON.stringify(newData));
+          setIsLoading(false);
           window.location.replace(`/ordersuccessful?${params.toString()}`);
         } else {
           const newData = response.data;
           const params = new URLSearchParams();
-          params.append("data", JSON.stringify(newData)); 
-          setIsLoading(false)
+          params.append("data", JSON.stringify(newData));
+          setIsLoading(false);
           window.location.replace(`/orderfail${params.toString()}`);
         }
       })
       .catch((error) => {
         console.log(error);
-        setIsLoading(false)
+        setIsLoading(false);
       });
   };
 
@@ -80,41 +86,45 @@ const Checkout = () => {
       <br />
       <div className="ilonasmedia2">
         <BackArrowMiddleHead content="Checkout" />
-        <div className='border' />
+        <div className="border" />
         <div className="one-order">
           <div className="flex-container">
             <div>My order</div>
           </div>
         </div>
 
-                <div className='border' />
-                <div className='one-order orderstuff'>
-                    <div className='flex-container'>
-                        <div className='smallerletters'>tröja</div>
-                        <div className='smallerletters'>1 x pris</div>
-                    </div>
-                    <div className='flex-container'>
-                        <div className='smallerletters'>tröja</div>
-                        <div className='smallerletters'>1 x pris</div>
-                    </div>
-                    <div className='flex-container'>
-                        <div className='smallerletters'>Discount</div> {/*om de finns promocode */}
-                        <div className='smallerletters'>-pris</div>
-                    </div>
-                    <div className='flex-container'>
-                        <div className='smallerletters'>Delivery</div>
-                        <div className='smallerletters FREE'>Free</div>
-                    </div>
-                </div>
+        <div className="border" />
+        <div className="one-order orderstuff">
+          <div className="flex-container">
+            <div className="smallerletters">tröja</div>
+            <div className="smallerletters">1 x pris</div>
+          </div>
+          <div className="flex-container">
+            <div className="smallerletters">tröja</div>
+            <div className="smallerletters">1 x pris</div>
+          </div>
+          <div className="flex-container">
+            <div className="smallerletters">Discount</div>{" "}
+            {/*om de finns promocode */}
+            <div className="smallerletters">-pris</div>
+          </div>
+          <div className="flex-container">
+            <div className="smallerletters">Delivery</div>
+            <div className="smallerletters FREE">Free</div>
+          </div>
+        </div>
 
-                <div className='border'/>
+        <div className="border" />
 
         <div className="one-order">
           <div className="flex-container">
             <div>Shipping details</div>
-                        <NavLink to="/checkout/shipping">
-                            <i class="fa-solid fa-chevron-right" style={{ color: "black" }}></i>
-                        </NavLink>
+            <NavLink to="/checkout/shipping">
+              <i
+                class="fa-solid fa-chevron-right"
+                style={{ color: "black" }}
+              ></i>
+            </NavLink>
           </div>
           <div className="flex-container">
             <div className="undertitle">
@@ -124,41 +134,44 @@ const Checkout = () => {
         </div>
 
         <div className="border" />
-                
+
         <div className="one-order">
           <div className="flex-container">
             <div>Payment Methods</div>
-                        <div className='arrow-icon'>
-                            <NavLink to="/checkout/payment">
-                                <i class="fa-solid fa-chevron-right" style={{ color: "black" }}></i>
-                            </NavLink>
-                        </div>          </div>
+            <div className="arrow-icon">
+              <NavLink to="/checkout/payment">
+                <i
+                  class="fa-solid fa-chevron-right"
+                  style={{ color: "black" }}
+                ></i>
+              </NavLink>
+            </div>{" "}
+          </div>
           <div className="flex-container">
             <div className="undertitle">7741 **** **** 6644</div>
           </div>
         </div>
-        <div className="border"/>
-        <br/>
+        <div className="border" />
+        <br />
 
-                <div className='inputcontainer '>
-                    <div className='text-center'>
-                        <InputSingel
-                            placeholder="Enter your comment"
-                            nameid="review"
-                            name="COMMENT"
-                            type="text"
-                            style="height: 140px;" 
-                        />
-                    </div>
-                </div>
-
-                <button className='basebtn checkoutbtn'>
-                    CONFIRM ORDER
-                </button>
-            </div>
-
+        <div className="inputcontainer ">
+          <div className="text-center">
+            <InputSingel
+              placeholder="Enter your comment"
+              nameid="review"
+              name="COMMENT"
+              type="text"
+              style="height: 140px;"
+            />
+          </div>
         </div>
-    )
-}
+
+        <button className="basebtn checkoutbtn" onClick={onSubmit}>
+          CONFIRM ORDER
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Checkout;
